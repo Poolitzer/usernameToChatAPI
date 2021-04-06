8a08b515f6fa1bded756a828862d3b68877ec606
poolitzer <25934244+Poolitzer@users.noreply.github.com>
2021-04-06 13:07:12 +0200

    feat: improving gitignore + checkURL
    gitignore handles the temporary files from telethon as well now, an error in checkURL leads to the telegram way of showing errors, and the text in Routes was increased.

--- checkURL.py
+++ checkURL.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 
 from log import increase_counter
 from api_keys import ALLOWED_KEYS
+from resolveUsername import create_error_response
 
 if TYPE_CHECKING:
     from typing import Mapping, Callable, Awaitable
@@ -26,13 +27,18 @@ async def check_url(
     # Request is thrown, providing the missing parameter
     for parameter in expected_parameters:
         if parameter not in request.rel_url.query:
-            error_string = "The required parameter " + parameter + " is missing."
-            raise web.HTTPBadRequest(reason=error_string)
+            error_string = parameter + " is missing."
+            return web.json_response(
+                data=create_error_response(400, error_string), status=400
+            )
     # if the api_key parameter is present, this part checks if the key is present in the list of keys. If it is not,
     # an Unauthorized error is thrown
     if "api_key" in expected_parameters:
         if request.rel_url.query["api_key"] not in ALLOWED_KEYS:
-            raise web.HTTPUnauthorized()
+            error_string = "Unauthorized"
+            return web.json_response(
+                data=create_error_response(401, error_string), status=400
+            )
     # this function call increases a counter for how many requests each api key did
     await increase_counter(request.rel_url.query["api_key"])
     # now the function which is supposed to handle the request gets the request, next to the three initiated objects,
